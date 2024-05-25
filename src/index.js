ae1073b01ef2213d7a12135116172709d8fddbb0
Ritesh Bisht <rb1342003@gmail.com>
2024-05-25 12:45:41 +0530

    MongoDB Connected

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
-console.log("index file");
- 
+//require('dotenv').config({path:'./env'})
+
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+//import {app} from './app.js'
+dotenv.config({
+    path: './.env'
+})
+
+
+
+connectDB()
